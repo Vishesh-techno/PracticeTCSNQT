0de0c367e81818b033340f14ca07903ffe46f4eb
Vishesh-techno <sonivishesh099@gmail.com>
2026-03-26 12:36:45 +0530

    Practice of Pyqs

--- src/PracticeSet1.java
+++ src/PracticeSet1.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class PracticeSet1 {
     static Scanner sc = new Scanner(System.in);
@@ -38,17 +35,80 @@ public class PracticeSet1 {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.print("Enter the size of Array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.print("Enter the " + n + " value of Array: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+    //    Array after k Rotations
+    public static void rotateArrayRight(int[] arr, int d) {
+        d = d % arr.length;
+        rotate(arr, 0, arr.length - 1);
+        rotate(arr, 0, d - 1);
+        rotate(arr, d, arr.length - 1);
+    }
+
+    //    Equilibrium Index
+    public static int equilibriumIndex(int[] arr) {
+        int leftSum = 0, rightSum = 0;
+        for (int i : arr) {
+            leftSum += i;
         }
-        System.out.println("The Array is: " + Arrays.toString(arr));
-        System.out.println("The first Non-Repeating Character in the Array is: " + firstNonRepeating(arr));
-        rotateArray(arr, 2);
-        System.out.print("The Rotated Array is: " + Arrays.toString(arr));
+        for (int i = arr.length - 1; i >= 0; i--) {
+            leftSum -= arr[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            rightSum += arr[i];
+        }
+        return -1;
+    }
+
+    //    Check if an array is subset of another array
+    public static boolean isSubset(int[] arr, int[] nums) {
+        Arrays.sort(arr);
+        Arrays.sort(nums);
+
+        int i = 0, j = 0, m = arr.length, n = nums.length;
+        while (i < m && j < n) {
+            if (arr[i] < nums[j]) {
+                i++;
+            } else if (arr[i] == nums[j]) {
+                i++;
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return j == n;
+    }
+
+    public static boolean isSubsetOptimal(int[] arr, int[] arr1) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        for (int i : arr1) {
+            if (!set.contains(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        System.out.print("Enter the size of Array: ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.print("Enter the " + n + " value of Array: ");
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("The Array is: " + Arrays.toString(arr));
+//        System.out.println("The first Non-Repeating Character in the Array is: " + firstNonRepeating(arr));
+//        rotateArray(arr, 2);
+//        System.out.print("The Rotated Array is: " + Arrays.toString(arr));
+        int[] arr = {1, 2, 0, 3};
+        System.out.println(equilibriumIndex(arr));
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        rotateArrayRight(arr1, 2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(isSubset(arr1, arr));
+        System.out.println(isSubsetOptimal(arr, arr1));
     }
 }
